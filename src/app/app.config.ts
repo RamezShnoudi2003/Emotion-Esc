54bcfb71b5817040cf2de3b001c1dcfa32869fc7
Ramez Shnoudi <ramezzshnoudii2003@gmail.com>
2025-05-08 15:45:14 +0300

    added withEnabledBlockingInitialNavigation() to routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import {
   provideZoneChangeDetection,
   isDevMode,
 } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import {
+  provideRouter,
+  RouterModule,
+  withEnabledBlockingInitialNavigation,
+  withHashLocation,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import {
@@ -25,7 +30,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
